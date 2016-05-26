0fe84da716c656c52e3744c66da3cd328eadbb80
Hoan Tran Thi Kim <hoan.tran@ext.magnolia-cms.com>
2016-05-26 17:54:31 +0700

    Add query builder class
    - Support criteria list query

---
+++ utility-library/src/test/java/com/hoantran/utility/sql/TestQueryBuilder.java
@@ -0,0 +1,29 @@
+/**
+ * This file Copyright (c) 2016 Magnolia International
+ * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
+ *
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Magnolia Network Agreement
+ * which accompanies this distribution, and is available at
+ * http://www.magnolia-cms.com/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package com.hoantran.utility.sql;
+
+import org.junit.Test;
+
+/**
+ * @author hoan.tran
+ */
+public class TestQueryBuilder {
+
+    @Test
+    public void buildCriteriaClause() {
+        System.out.println(String.format("test %s, %s", "abc", "xyz"));
+    }
+
+}
