aba996730aa7fe9166241f0cc1dca8d431c20876
Hoan Tran <hoantran.it@gmail.com>
2017-10-03 14:19:11 +0700

    Update image scaling

---
+++ utility-library/src/main/java/com/github/hoantran/lib/utility/image/ImageScaling.java
@@ -0,0 +1,115 @@
+/**
+ * Created by Hoan Tran @ http://hoantran-it.blogspot.com
+ *
+ * Any modifications to this file must keep this entire header intact.
+ *
+ */
+package com.github.hoantran.lib.utility.image;
+
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+
+/**
+ * Image scaling.
+ * 
+ * @author HoanTran
+ */
+public class ImageScaling {
+
+    public static final String DEFAULT_IMAGE_FORMAT = "jpg";
+
+    public static final boolean KEEP_ASPECT_RATIO = true;
+
+    public static final int DEFAULT_IMAGE_TYPE = BufferedImage.TYPE_INT_RGB;
+
+    public static InputStream resizeImage(String imageUrl, int newWidth, int newHeight) throws IOException {
+        return resizeImage(new URL(imageUrl).openStream(), newWidth, newHeight, KEEP_ASPECT_RATIO, DEFAULT_IMAGE_FORMAT);
+    }
+
+    public static InputStream resizeImage(String imageUrl, int newWidth, int newHeight, boolean keepAspectRatio) throws IOException {
+        return resizeImage(new URL(imageUrl).openStream(), newWidth, newHeight, keepAspectRatio, DEFAULT_IMAGE_FORMAT);
+    }
+
+    public static InputStream resizeImage(String imageUrl, int newWidth, int newHeight,
+            boolean keepAspectRatio, String imageFormat) throws IOException {
+        return resizeImage(new URL(imageUrl).openStream(), newWidth, newHeight, keepAspectRatio, imageFormat);
+    }
+
+    public static InputStream resizeImage(File file, int newWidth, int newHeight) throws IOException {
+        return resizeImage(new FileInputStream(file), newWidth, newHeight, KEEP_ASPECT_RATIO, DEFAULT_IMAGE_FORMAT);
+    }
+    
+    public static InputStream resizeImage(File file, int newWidth, int newHeight, boolean keepAspectRatio) throws IOException {
+        return resizeImage(new FileInputStream(file), newWidth, newHeight, keepAspectRatio, DEFAULT_IMAGE_FORMAT);
+    }
+
+    public static InputStream resizeImage(File file, int newWidth, int newHeight,
+            boolean keepAspectRatio, String imageFormat) throws IOException {
+        return resizeImage(new FileInputStream(file), newWidth, newHeight, keepAspectRatio, imageFormat);
+    }
+
+    public static InputStream resizeImage(InputStream input, int newWidth, int newHeight) throws IOException {
+        return resizeImage(input, newWidth, newHeight, KEEP_ASPECT_RATIO, DEFAULT_IMAGE_FORMAT);
+    }
+
+    public static InputStream resizeImage(InputStream input, int newWidth, int newHeight, boolean keepAspectRatio) throws IOException {
+        return resizeImage(input, newWidth, newHeight, keepAspectRatio, DEFAULT_IMAGE_FORMAT);
+    }
+
+    public static InputStream resizeImage(InputStream input, int newWidth, int newHeight,
+            boolean keepAspectRatio, String imageFormat) throws IOException {
+        BufferedImage originalImage = ImageIO.read(input);
+        BufferedImage resizedImage = performImageScaling(originalImage, newWidth, newHeight, keepAspectRatio, imageFormat);
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(resizedImage, imageFormat, baos);
+        return new ByteArrayInputStream(baos.toByteArray());
+    }
+
+    public static BufferedImage performImageScaling(BufferedImage originalImage, int newWidth, int newHeight,
+            boolean keepAspectRatio, String imageFormat) throws IOException {
+        if (keepAspectRatio) {
+            return resizeImageKeepAspectRatio(originalImage, newWidth, newHeight, imageFormat);
+        } else {
+            return resizeImageAbsolutely(originalImage, newWidth, newHeight, imageFormat);
+        }
+    }
+
+    public static BufferedImage resizeImageKeepAspectRatio(BufferedImage originalImage, int newWidth, int newHeight, String imageFormat) throws IOException {
+        double determineImageScale = determineImageScale(originalImage.getWidth(), originalImage.getHeight(), newWidth, newHeight);
+        Image scaledImage = originalImage.getScaledInstance((int) (originalImage.getWidth() * determineImageScale),
+                (int) (originalImage.getHeight() * determineImageScale), Image.SCALE_SMOOTH);
+        BufferedImage resizedImage = new BufferedImage(scaledImage.getWidth(null), scaledImage.getHeight(null),
+                DEFAULT_IMAGE_TYPE);
+        Graphics2D g = resizedImage.createGraphics();
+        g.drawImage(scaledImage, 0, 0, null);
+        g.dispose();
+        return resizedImage;
+    }
+
+    public static BufferedImage resizeImageAbsolutely(BufferedImage originalImage, int newWidth, int newHeight, String imageFormat) throws IOException {
+        BufferedImage resizedImage = new BufferedImage(newWidth, newHeight,
+                DEFAULT_IMAGE_TYPE);
+        Graphics2D g = resizedImage.createGraphics();
+        g.drawImage(originalImage, 0, 0, newWidth, newHeight, null);
+        g.dispose();
+        return resizedImage;
+    }
+
+    private static double determineImageScale(int sourceWidth, int sourceHeight, int targetWidth, int targetHeight) {
+        double scalex = (double) targetWidth / sourceWidth;
+        double scaley = (double) targetHeight / sourceHeight;
+        return Math.min(scalex, scaley);
+    }
+
+}
